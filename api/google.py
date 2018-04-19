ea4970d8d59ff7d4b93a3329a8b7d04908e50336
Timky <itimky@gmail.com>
2018-04-19 12:30:16 +0300

    use fake user agent for google

--- api/google.py
+++ api/google.py
@@ -1,9 +1,11 @@
 import requests
 from flask_restful import reqparse, Resource
+from fake_useragent import UserAgent
 
 from . import utils
 
 
+UA = UserAgent()
 HTTP_429_TOO_MANY_REQUESTS = 429
 RETRIES = 1
 GOOGLE_URL = 'https://www.google.ru/search?num=100&q=%s&near=%s'
@@ -41,9 +43,13 @@ class Google(Resource):
         """
         args = get_query_parser().parse_args()
         link = GOOGLE_URL % (args.query, args.region)
+        headers = requests.utils.default_headers()
+        headers.update({
+            'User-Agent': UA.random
+        })
 
         for i in range(RETRIES):
-            r = requests.get(link, proxies=PROXIES)
+            r = requests.get(link, proxies=PROXIES, headers=headers)
             if 'google.com/sorry/' not in r.url:
                     return {'soup': utils.get_soup(r.text)}
         return {'error': 'Still captcha after %s retries' % RETRIES},\
